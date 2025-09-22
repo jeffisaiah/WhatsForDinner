d8de3b329506690921bbe9095607ad1532cca648
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-09-22 17:52:04 -0400

    09/22/25 In class changes

--- WhatsForDinner/ContentView.swift
+++ WhatsForDinner/ContentView.swift
@@ -8,12 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let choicesArray = ["BBQ",
+                                       "Burgers",
+                                       "Burritos",
+                                       "Chinese",
+                                       "Fried Chicken",
+                                       "Pasta",
+                                       "Pizza",
+                                       "Ramen",
+                                       "Salad",
+                                       "Steak",
+                                       "Sushi",
+                                       "Thai"]
+    @State private var selectedMeal = "Thai"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Text("What's For Dinner?")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.cyan)
+            
+            Spacer()
+            
+            Image(selectedMeal)
+                .resizable()
+                .scaledToFit()
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .animation(.default, value: selectedMeal)
+            
+            
+            Text(selectedMeal)
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .animation(.default, value: selectedMeal)
+            
+            Spacer()
+            
+            Button{
+                var currentMeal: String
+                repeat{
+                    currentMeal = choicesArray.randomElement() ?? "No Meals Availible"
+                } while currentMeal == selectedMeal
+                selectedMeal = currentMeal
+            
+            } label: {
+                Image(systemName: "fork.knife.circle")
+                Text("What's For Dinner?")
+            }
+            .font(.title)
+            .fontWeight(.semibold)
+            .buttonStyle(.glassProminent)
+            .tint(.cyan)
         }
         .padding()
     }
