4e16701c4f4dddca7b442c0e27ff7d1437554636
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-09-22 17:02:08 -0400

    Initial Commit

---
+++ WhatsForDinner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsForDinner
+//
+//  Created by Jeff Gutierrez on 9/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsForDinner/WhatsForDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsForDinnerApp.swift
+//  WhatsForDinner
+//
+//  Created by Jeff Gutierrez on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatsForDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
